dd8402c7828938449346a8fff16dcf2449c37246
Traci Williams <traciwilliams@Tracis-MacBook-Pro.local>
2016-10-19 12:28:07 -0500

    added a change

--- controllers/user.js
+++ controllers/user.js
@@ -16,17 +16,24 @@ var users = [
 		{
 		username: 'Testy McTestingSon',
 		password: 'l33tp4zzw0rdz'
-
 	}	
-
 ]
-
-
 
 
 ctrl.get('/', function(req, res, next){ //request //response //callback
 	res.json(users);
+});
 
+ctrl.get('/1', function(req, res, next){
+	res.json(users[0]);
+});
+
+ctrl.get('/2', function(req, res, next){
+	res.json(users[1]);
+});
+
+ctrl.get('/3', function(req, res, next){
+	res.json(users[2]);
 });
 
 
